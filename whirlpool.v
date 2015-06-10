c5af7248bcc4f534de4784c36a70613417bdcd40
cryptoflyr <cryptoflyr@gmail.com>
2015-06-10 16:18:07 -0400

    Update whirlpool.v

--- whirlpool.v
+++ whirlpool.v
@@ -28,22 +28,69 @@ module whirlpool (
 	output [511:0] hash
 );
 
-	wire [511:0] b0,b1,b2,b3,b4,b5,b6,b7,b8,b9;
-	wire [511:0] k0,k1,k2,k3,k4,k5,k6,k7,k8,k9;
+	wire [63:0] ROUND_CONSTANT [0:9] = {
+		64'h1823C6E887B8014F, 64'h36A6D2F5796F9152, 64'h60BC9B8EA30C7B35, 64'h1DE0D7C22E4BFE57, 64'h157737E59FF04ADA, 
+		64'h58C9290AB1A06B85, 64'hBD5D10F4CB3E0567, 64'hE427418BA77D95D8,	64'hFBEE7C66DD17479E, 64'hCA2DBF07AD5A8333	
+	};
 	
-	process_round r0 ({352'd0, block}, state, 64'h1823C6E887B8014F, b0, k0);
-	process_round r1 (b0, k0, 64'h36A6D2F5796F9152, b1, k1);
-	process_round r2 (b1, k1, 64'h60BC9B8EA30C7B35, b2, k2);
-	process_round r3 (b2, k2, 64'h1DE0D7C22E4BFE57, b3, k3);
-	process_round r4 (b3, k3, 64'h157737E59FF04ADA, b4, k4);
-	process_round r5 (b4, k4, 64'h58C9290AB1A06B85, b5, k5);
-	process_round r6 (b5, k5, 64'hBD5D10F4CB3E0567, b6, k6);
-	process_round r7 (b6, k6, 64'hE427418BA77D95D8, b7, k7);
-	process_round r8 (b7, k7, 64'hFBEE7C66DD17479E, b8, k8);
-	process_round r9 (b8, k8, 64'hCA2DBF07AD5A8333, b9, k9);
+	reg [511:0] block_q, state_q;		// Temp Storage For State & Block Rounds Between Clock Cycles
+	reg [511:0] round_in;				// Block Input For The Current Round
+	reg [511:0] round_key;				// Key Input For The Current Round
+	wire [511:0] round_out;				// Output For The Current Round
 
-	assign hash = state[511:480] ^ b9[511:480];
+	reg [3:0] round_cnt = 4'd0;		// Current Round (0 to 9)
+	reg process_state = 1'b1;			// Indicator To Indentify If State Or Block Round Is Being Processed
+	reg hash_done = 1'b0;				// Indicator For When All 10 Rounds Are Complete
 	
-	assign hash_ready = 1'b1;
+	// Process Rounds
+	process_round round (round_in, round_key, round_out);
 
+	assign hash = state ^ block ^ round_out;
+	assign hash_ready = hash_done;
+
+	always @ (posedge clk) begin
+	
+		hash_done = 1'b0;
+
+		if (rst)	begin										// Reset The Data For The Next 10 Rounds
+			round_cnt = 4'd0;
+			process_state = 1'b1;
+		end
+	
+		if (process_state) begin						// State Round
+
+			round_key = {ROUND_CONSTANT[round_cnt], 448'd0};
+			block_q = round_out;
+
+			if (round_cnt == 4'd0)						// Reset The State Round To Use The Midstate
+				round_in = state;
+			else
+				round_in = state_q;
+		end
+		else begin											// Block Round
+		
+			round_key = round_out;
+			state_q = round_out;
+
+			if (round_cnt == 4'd0)						// Reset The Block Round To Use The Work Block XOR'd With Midstate Data
+				round_in = block ^ state;
+			else
+				round_in = block_q;
+
+			// Increment Round Once Both State & Block Rounds Complete
+			round_cnt = round_cnt + 1'b1;
+
+			if (round_cnt == 4'd10) begin				// Notify Work Handler That Hash Is Complete
+				round_cnt = 4'd0;
+				hash_done = 1'b1;
+			end
+
+		end
+		
+		process_state = ~process_state;
+
+//		$display("Round: %d, Rst: %d, P_State: %d, Ready: %d, Round Out: %x", round_cnt, rst, process_state, hash_ready, round_out);
+	
+	end
+	
 endmodule
